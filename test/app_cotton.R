a1cb2f6618e6a39b822907c33b4703f134be811f
Comfort Adegbenro <coa07030@uga.edu>
2025-04-22 11:07:52 -0400

    simple shiny app created

---
+++ test/app_cotton.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(shiny)
+
+weather = read.csv("weather_monthsum.csv")
+
+ui = fluidPage(
+  
+  #Title
+  titlePanel("Cotton EDA"),
+  
+  #Sidebar with a slider
+  sidebarLayout(
+    sidebarPanel(
+      varSelectInput("variable",
+                     "Variable:",
+                     weather)
+    ),
+    mainPanel(
+      plotOutput("Plot")
+    )
+  )
+)
+
+
+server = function(input, output) {
+  output$Plot = renderPlot({
+    ggplot(data = weather,
+           aes(x = !!sym(input$variable))) +
+             geom_density()
+  })
+}
+
+shinyApp(ui, server)
